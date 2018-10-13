0baed10f85b61f51b2503803c04fcbd782f31593
zhengyicai <1010172623@qq.com>
2018-10-13 19:03:27 +0800

    create

---
+++ src/api/api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+let base = '';
+export const url  = "http://127.0.0.1:8080/cms";
+
+export const requestLogin = params => { return axios.post(`${base}/login`, params).then(res => res.data); };
+
+export const getUserList = params => { return axios.get(`${base}/user/list`, { params: params }); };
+
+export const getUserListPage = params => { return axios.get(`${base}/user/listpage`, { params: params }); };
+
+export const removeUser = params => { return axios.get(`${base}/user/remove`, { params: params }); };
+
+export const batchRemoveUser = params => { return axios.get(`${base}/user/batchremove`, { params: params }); };
+
+export const editUser = params => { return axios.get(`${base}/user/edit`, { params: params }); };
+
+export const addUser = params => { return axios.get(`${base}/user/add`, { params: params }); };
